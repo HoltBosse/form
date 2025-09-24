6f3e585c46e521fc35ff59273efc94bcb415a091
workaj <109080500+workaj@users.noreply.github.com>
2025-09-24 13:24:58 -0400

    chore: make sure nowrap is a bool

--- src/Field.php
+++ src/Field.php
@@ -124,7 +124,7 @@ class Field {
 		$this->minlength = $config->minlength ?? 0;
 		$this->placeholder = $config->placeholder ?? "";
 		$this->logic = $config->logic ?? '';
-		$this->nowrap = $config->nowrap ?? null;
+		$this->nowrap = $config->nowrap ?? false;
 		$this->form = $config->form ?? null;
 	}
 }
