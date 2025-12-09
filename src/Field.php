911af2a60ac83e052dc5fd26c1e74c6d2021be07
workaj <109080500+workaj@users.noreply.github.com>
2025-12-09 11:16:13 -0500

    chore: add thing

--- src/Field.php
+++ src/Field.php
@@ -112,10 +112,12 @@ class Field {
 		// ostensibly used by 'list' item option in content listings for user driven columns
 		// helpful info can be anything, but something like the field config object
 		// can be used to determine, for example, a content type for a contentselector etc
+		$output = $this->default;
+		$output = htmlspecialchars_decode($output); //due to some old junky filters, stuff was stored encoded, so decode it first
 		if($helpfulInfo && $helpfulInfo->return_in_text_html_form==true) {
-			return Input::stringHtmlSafe($this->default);
+			return Input::stringHtmlSafe($output);
 		} else {
-			return $this->default;
+			return $output;
 		}
 	}
 
